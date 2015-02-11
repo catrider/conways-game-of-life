e6edfedff6c0b56db96181011c6873da4fde3edb
Daniel Fischer <d.fischer874@gmail.com>
2015-02-10 22:51:38 -0500

    Implemented display-grid function

--- src/conways_game_of_life/grid_display.clj
+++ src/conways_game_of_life/grid_display.clj
@@ -1,5 +1,20 @@
-(ns conways-game-of-life.grid-display)
+(ns conways-game-of-life.grid-display
+  (:require [conways-game-of-life.grid :as g]))
+
+(defn- thread-last-conj
+  [e vec]
+  (conj vec e))
+
+(defn- grid-border
+  [grid]
+  (vec (flatten (repeat (g/width grid) (list \space \-)))))
 
 (defn display-grid
   [grid]
-  ())
+  (->> (map (fn [row] (map (fn [v] (case v 0 \space 1 \*)) row)) grid)
+       (map (comp (partial cons \|) #(conj % \| \newline) vec (partial interpose \space)))
+       vec
+       (thread-last-conj (grid-border grid))
+       (cons (conj (grid-border grid) \newline))
+       flatten
+       (apply str)))
--- test/conways_game_of_life/grid_display_test.clj
+++ test/conways_game_of_life/grid_display_test.clj
@@ -3,9 +3,33 @@
             [conways-game-of-life.grid-display :refer :all]))
 
 (deftest display-grid-test
-  (testing "The grid is displayed"
-    (is (= " - - - -\n|  *   *|\n|*      |\n|  * *  |\n|* *   *|\n - - - -"
+  (testing "a 4x4 grid is displayed"
+    (is (= (str " - - - -\n"
+                "|  * * *|\n"
+                "|*     *|\n"
+                "|      *|\n"
+                "|  * *  |\n"
+                " - - - -" )
            (display-grid [[0 1 1 1]
                           [1 0 0 1]
                           [0 0 0 1]
-                          [0 1 1 0]])))))
+                          [0 1 1 0]]))))
+  (testing "an 8x10 grid is displayed"
+    (is (= (str " - - - - - - - - - -\n"
+                "|  * * *   * * *   *|\n"
+                "|*     *     * *    |\n"
+                "|      *   * * * *  |\n"
+                "|  * *   *     *   *|\n"
+                "|*     * *   * *    |\n"
+                "|* *     * *   *    |\n"
+                "|  *         *     *|\n"
+                "|* *     *     * * *|\n"
+                " - - - - - - - - - -")
+           (display-grid [[0 1 1 1 0 1 1 1 0 1]
+                          [1 0 0 1 0 0 1 1 0 0]
+                          [0 0 0 1 0 1 1 1 1 0]
+                          [0 1 1 0 1 0 0 1 0 1]
+                          [1 0 0 1 1 0 1 1 0 0]
+                          [1 1 0 0 1 1 0 1 0 0]
+                          [0 1 0 0 0 0 1 0 0 1]
+                          [1 1 0 0 1 0 0 1 1 1]])))))
